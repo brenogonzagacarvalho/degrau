776bd77febd936682385d2ec49ea858b89d08424
Breno Gonzaga de Carvalho <31925096+brenogonzagacarvalho@users.noreply.github.com>
2022-10-31 18:53:18 -0300

    Ajustando projeto

---
+++ app/src/main/java/com/degrau/RecuperarContaActivity.java
@@ -0,0 +1,14 @@
+package com.degrau;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecuperarContaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar_conta);
+    }
+}
--- app/src/main/java/com/degrau/SplashActivity.java
+++ app/src/main/java/com/degrau/SplashActivity.java
@@ -2,7 +2,9 @@ package com.degrau;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler(getMainLooper()).postDelayed(() -> {
+                finish();
+        startActivity(new Intent(this, LoginActivity.class));
+      }, 3000);
+
     }
 }
